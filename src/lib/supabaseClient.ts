7030f1105f9e97f287b0f05d3a0c2dfc3c84c36d
jhonkeithman123 <keithvirgenes17@gmail.com>
2026-03-23 17:53:20 +0800

    Lets see

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,10 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = process.env.SUPABASE_URL || "";
 const SUPABASE_KEY =
   process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_ANON_KEY || "";
 
-export const supabase: SupabaseClient | null =
+export const supabase: ReturnType<typeof createClient> | null =
   SUPABASE_URL && SUPABASE_KEY
     ? createClient(SUPABASE_URL, SUPABASE_KEY)
     : null;
